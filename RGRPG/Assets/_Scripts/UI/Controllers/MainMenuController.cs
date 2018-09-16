d9c649b75106b10bffaaf6595d29aa9120704e8c
Craig <craigwstone@gmail.com>
2018-09-16 16:14:09 -0400

    Added Main Menu UI and FPS Counter

---
+++ RGRPG/Assets/_Scripts/UI/Controllers/MainMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    public GameObject StartButtonObject;
+    public GameObject OptionsButtonObject;
+    public GameObject CreditsButtonObject;
+    public GameObject QuitButtonObject;
+
+    private Button StartButton;
+    private Button OptionsButton;
+    private Button CreditsButton;
+    private Button QuitButton;
+
+
+	void Start ()
+    {
+        StartButton = StartButtonObject.GetComponent<Button>();
+        OptionsButton = OptionsButtonObject.GetComponent<Button>();
+        CreditsButton = CreditsButtonObject.GetComponent<Button>();
+        QuitButton = QuitButtonObject.GetComponent<Button>();
+
+        StartButton.onClick.AddListener(() => {
+            SceneManager.LoadScene("GameScene");
+        });
+        OptionsButton.onClick.AddListener(() => {
+            SceneManager.LoadScene("OptionsScene");
+        });
+        CreditsButton.onClick.AddListener(() => {
+            SceneManager.LoadScene("CreditsScene");
+        });
+        QuitButton.onClick.AddListener(() => {
+            Application.Quit();
+        });
+    }
+	
+	void Update ()
+    {
+
+	}
+}
---
+++ RGRPG/Assets/_Scripts/UI/Controllers/OptionsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsController : MonoBehaviour
+{
+    public GameObject BackButtonObject;
+
+    private Button BackButton;
+
+    void Start()
+    {
+        BackButton = BackButtonObject.GetComponent<Button>();
+
+        BackButton.onClick.AddListener(() => {
+            SceneManager.LoadScene("MainMenuScene");
+        });
+    }
+
+    void Update()
+    {
+
+    }
+}
